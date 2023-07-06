bbc975ff0785ca8825a6fe868a776a9cbbee2192
bluuo <michael.barborka@hotmail.com>
2023-07-06 09:49:33 +0200

    added local DB connection show data in listbox

--- QuizMaker/Screens/QuestionManager.cs
+++ QuizMaker/Screens/QuestionManager.cs
@@ -10,11 +10,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Npgsql;
+using System.Data.SqlClient;
 
 namespace QuizMaker.Screens
 {
     public partial class QuestionManager : MaterialForm
     {
+        //private string connectionString = Properties.Settings.Default.dbConnection;
         public QuestionManager()
         {
             InitializeComponent();
@@ -39,6 +42,40 @@ namespace QuizMaker.Screens
             TextboxCategory.AutoCompleteMode = AutoCompleteMode.Suggest;
             TextboxCategory.AutoCompleteSource = AutoCompleteSource.CustomSource;
 
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\micha\\Source\\Repos\\bluuo\\QuizMaker\\QuizMaker\\Questions.mdf;Integrated Security=True"; // Replace with your actual connection string
+           
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM QuestionsTable"; // Replace with your actual table name
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Retrieve values for each row
+                            int id = reader.IsDBNull(0) ? 0 : reader.GetInt32(0);
+                            string category = reader.IsDBNull(1) ? string.Empty : reader.GetString(1);
+                            string question = reader.IsDBNull(2) ? string.Empty : reader.GetString(2);
+                            string answer_correct = reader.IsDBNull(3) ? string.Empty : reader.GetString(3);
+                            string answer_wrong1 = reader.IsDBNull(4) ? string.Empty : reader.GetString(4);
+                            string answer_wrong2 = reader.IsDBNull(5) ? string.Empty : reader.GetString(5);
+                            string answer_wrong3 = reader.IsDBNull(6) ? string.Empty : reader.GetString(6);
+
+                            // Create a formatted string to represent the row
+                            string rowText = $"{id} {category} {question}";
+
+                            // Add the row to the ListBox
+                            ListboxQuestions.Items.Add(rowText);
+                        }
+                    }
+                }
+            }
+
         }
 
         private void materialMultiLineTextBox21_Click(object sender, EventArgs e)
@@ -48,23 +85,34 @@ namespace QuizMaker.Screens
 
         private void ButtonAddQuestion_Click(object sender, EventArgs e)
         {
-            FragenDB.FragenTabelleDataTable fragenDB = new FragenDB.FragenTabelleDataTable();
-            FragenDB.FragenTabelleRow newRow = fragenDB.NewFragenTabelleRow();
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\micha\\Source\\Repos\\bluuo\\QuizMaker\\QuizMaker\\Questions.mdf;Integrated Security=True"; // Replace with your actual connection string
 
-            newRow.category = TextboxCategory.Text;
-            newRow.question = TextboxQuestion.Text;
-            newRow.correct_answer = TextboxCorrect.Text;
-            newRow.wrong_answer1 = TextboxWrong1.Text;
-            newRow.wrong_answer2 = TextboxWrong2.Text;
-            newRow.wrong_answer3 = TextboxWrong3.Text;
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = connection.CreateCommand();
+            connection.Open();
+            command.CommandText =
+                "Insert into QuestionsTable(category, question, answer_correct, answer_wrong1, answer_wrong2, answer_wrong3) " +
+                "VALUES ('value1', 'value2', 'value3', 'value4', 'value5', 'value6')";
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+            MessageBox.Show("Record Submitted", "Congrats");
+            connection.Close();
 
-            fragenDB.AddFragenTabelleRow(newRow);
-            fragenDB.AcceptChanges();
         }
 
         private void ListboxQuestions_SelectedIndexChanged(object sender, MaterialListBoxItem selectedItem)
         {
 
+        }
+
+        private void ListboxQuestions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialButton1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
